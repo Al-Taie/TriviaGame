b9b335575f6b9d609429b665e68537ac5f1190cc
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2021-09-22 02:33:47 +0300

    create client class

---
+++ app/src/main/java/com/altaie/triviagame/network/Client.kt
@@ -0,0 +1,46 @@
+package com.altaie.triviagame.network
+
+import android.util.Log
+import com.altaie.triviagame.data.category.NationalCategoryResponse
+import com.altaie.triviagame.util.Parser
+import com.google.gson.Gson
+import okhttp3.*
+import java.io.IOException
+
+object Client {
+    private val okHttpClient = OkHttpClient()
+    private val url = "https://opentdb.com/api_category.php"
+    private val gson = Gson()
+
+
+    fun getCategory() {
+        val request = Request.Builder().url(url).build()
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.v("request_category", "fail: ${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.body?.string().let { categorySting ->
+                    val result = gson.fromJson(categorySting, NationalCategoryResponse::class.java)
+                    Log.v("test", result.toString())
+
+                }
+            }
+        })
+    }
+
+    fun getQuestion(request: Request) {
+        okHttpClient.newCall(request = request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.v("FAILURE_REQUEST", e.message.toString())
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.body?.let { Parser.parse(it.string()) }
+
+            }
+        })
+    }
+
+}
