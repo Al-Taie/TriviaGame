ff43f25171fd68629e2d4fee4955ae0d72ff0291
Ahmed Yusef <ahmed.yusef.1993@gmail.com>
2021-09-23 00:49:34 +0300

    fix: button return to home fragment in result fragment

--- app/src/main/java/com/altaie/triviagame/ui/result/ResultFragment.kt
+++ app/src/main/java/com/altaie/triviagame/ui/result/ResultFragment.kt
@@ -3,55 +3,78 @@ package com.altaie.triviagame.ui.result
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat.getColor
+import androidx.fragment.app.Fragment
 import com.altaie.triviagame.R
 import com.altaie.triviagame.databinding.FragmentResultBinding
 import com.altaie.triviagame.ui.base.BaseFragment
+import com.altaie.triviagame.ui.home.HomeFragment
 import com.altaie.triviagame.util.Constant
 
 
 class ResultFragment : BaseFragment<FragmentResultBinding>() {
-
+    lateinit var myHomeFragment: HomeFragment
     override fun onStart() {
         super.onStart()
         arguments?.let {
-            val result = it.getString(Constant.RESULT_KEY)
-            binding.result.text = "$result\\100"
-            result?.let { it1 -> resultStatus(it1.toInt()) }
+            val score = it.getString(Constant.RESULT_KEY)
+            binding.result.text = "$score\\100"
+            score?.let { it1 -> resultStatus(it1.toInt()) }
         }
 
     }
-    private fun resultStatus(result: Int){
+    private fun resultStatus(score: Int){
+        binding.apply {
         when {
-            result >= 90 -> {
-                binding.resultStatus.text = "Excellent"
-                binding.resultStatus.setTextColor(getColor(requireContext(), R.color.dark_green))
+            score >= 90 -> {
+                resultStatus.text = "Excellent"
+                resultStatus.setTextColor(getColor(requireContext(), R.color.dark_green))
+                result.setTextColor(getColor(requireContext(), R.color.dark_green))
             }
-            result >= 80 -> {
-                binding.resultStatus.text = "Very Good"
-                binding.resultStatus.setTextColor(getColor(requireContext(), R.color.purple_700))
+            score >= 80 -> {
+                resultStatus.text = "Very Good"
+                resultStatus.setTextColor(getColor(requireContext(), R.color.purple_700))
+                result.setTextColor(getColor(requireContext(), R.color.purple_700))
             }
-            result >= 70 -> {
-                binding.resultStatus.text = "Good"
-                binding.resultStatus.setTextColor(getColor(requireContext(), R.color.purple_500))
+            score >= 70 -> {
+                resultStatus.text = "Good"
+                resultStatus.setTextColor(getColor(requireContext(), R.color.purple_500))
+                result.setTextColor(getColor(requireContext(), R.color.purple_500))
             }
-            result >= 60 -> {
-                binding.resultStatus.text = "Medium"
-                binding.resultStatus.setTextColor(getColor(requireContext(), R.color.blue))
+            score >= 60 -> {
+                resultStatus.text = "Medium"
+                resultStatus.setTextColor(getColor(requireContext(), R.color.blue))
+                result.setTextColor(getColor(requireContext(), R.color.blue))
             }
-            result >= 50 -> {
-                binding.resultStatus.text = "Passable"
-                binding.resultStatus.setTextColor(getColor(requireContext(), R.color.darkGrey))
+            score >= 50 -> {
+                resultStatus.text = "Passable"
+                resultStatus.setTextColor(getColor(requireContext(), R.color.darkGrey))
+                result.setTextColor(getColor(requireContext(), R.color.darkGrey))
             }
             else -> {
-                binding.resultStatus.text = "Weak"
-                binding.resultStatus.setTextColor(getColor(requireContext(), R.color.dark_red))
+                resultStatus.text = "Weak"
+                resultStatus.setTextColor(getColor(requireContext(), R.color.dark_red))
+                result.setTextColor(getColor(requireContext(), R.color.dark_red))
             }
+        }
         }
     }
 
     override fun setup() {}
 
-    override fun callBack() {}
+    override fun callBack() {
+        binding.returnHomeBtn.setOnClickListener {
+            myHomeFragment = HomeFragment()
+            replaceFragment(myHomeFragment)
+        }
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        activity?.let {
+            it.supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment_container, fragment)
+                addToBackStack(null)
+            }.commit()
+        }
+    }
 
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentResultBinding
         get() = FragmentResultBinding::inflate
